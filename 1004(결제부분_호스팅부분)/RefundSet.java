a7a2ac3d820c94ae5f7e05ee4bdae87c1366c9d9
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-10-04 17:30:15 +0900

    Add files via upload

---
+++ 1004(결제부분_호스팅부분)/RefundSet.java
@@ -0,0 +1,11 @@
+package com.bam.setting;
+
+public class RefundSet {
+	public static final String IMPORT_TOKEN_URL = "https://api.iamport.kr/users/getToken"; 
+	public static final String IMPORT_PAYMENTINFO_URL = "https://api.iamport.kr/payments/find/"; 
+	public static final String IMPORT_CANCEL_URL = "https://api.iamport.kr/payments/cancel"; 
+	public static final String IMPORT_PREPARE_URL = "https://api.iamport.kr/payments/prepare";
+
+	public static final String KEY = "8887223726532468";
+	public static final String SECRET = "O1lddk6Mo9qcAPnK8cxojwbcLDqB01EdXusi4bpDRQJsf7OP8sITmMY8vN9Fu34yEmhV3zNstxFFrWxQ";
+}

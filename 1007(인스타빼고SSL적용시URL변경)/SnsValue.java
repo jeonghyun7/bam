7da5a52d3ac5101095794719c586d81c6756b6be
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-10-07 12:49:25 +0900

    Add files via upload

---
+++ 1007(인스타빼고SSL적용시URL변경)/SnsValue.java
@@ -0,0 +1,38 @@
+package com.bam.auth;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+
+import lombok.Data;
+
+@Data
+public class SnsValue implements SnsUrl{
+	private String service;
+	private String clientId;
+	private String clientSecret;
+	private String redirectUrl;
+	private DefaultApi20 api20Instance;
+	private String profileUrl;
+	
+	private boolean isNaver;
+	
+	public SnsValue(String service) {
+		this.service = service;
+		this.clientId = "dJm1Vk_5EICTBvv2wmMN";
+		this.clientSecret = "EPA15VyeXa";
+		this.redirectUrl = "https://localhost:8443/login/naver/callback";
+		if(service.equals("naver")) this.isNaver = true;
+		if(isNaver) {
+			this.api20Instance = NaverApi20.instance();
+			this.profileUrl = NAVER_PROFILE_URL;
+			
+//		this.service = service;
+//	    this.clientId = "[클라이언트 아이디]";
+//	    this.clientSecret = "[시크릿 키]";
+//	    this.redirectUrl = "https://3.35.93.88:8443/login/naver/callback";
+//	    if(service.equals("naver")) this.isNaver = true;
+//	    if(isNaver) {
+//	         this.api20Instance = NaverApi20.instance();
+//	         this.profileUrl = NAVER_PROFILE_URL;	
+		}
+	}
+}
